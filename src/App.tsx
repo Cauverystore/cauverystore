730471fd4e9b2d6d3a1fda6050ab6800d7ae7dcf
Cauverystore <papillonmediaworks@gmail.com>
2025-05-25 17:56:52 +0530

    Fix: Add .tsx extension to Storefront import for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Storefront from './pages/Storefront.tsx';
+import Storefront from "./pages/Storefront.tsx";
 import Cart from "@/pages/Cart";
 import Checkout from "@/pages/Checkout";
 import CustomerOrdersPage from "@/pages/CustomerOrdersPage";
