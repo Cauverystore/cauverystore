f330d2d72cdc4767acad27d57405da5a9da382e0
Cauverystore <papillonmediaworks@gmail.com>
2025-05-25 21:38:36 +0530

    Set up routing and load Storefront as home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Storefront from './pages/storefront';
+import Storefront from './pages/Storefront';
 import Cart from "@/pages/Cart";
 import Checkout from "@/pages/Checkout";
 import CustomerOrdersPage from "@/pages/CustomerOrdersPage";
@@ -8,26 +8,23 @@ import MerchantDashboard from "@/pages/MerchantDashboard";
 import AdminOrdersPage from "@/pages/AdminOrdersPage";
 import AddProduct from "@/pages/AddMerchantProduct";
 import EditProduct from "@/pages/EditMerchantProduct";
-import Navbar from "@/components/Navbar"; // includes NavCart
+import Navbar from "@/components/Navbar";
 
 function App() {
   return (
     <Router>
-      {/* Toast notifications */}
-      <Toaster position="top-right" />
-
-      {/* Main Layout */}
       <Navbar />
-
+      <Toaster />
       <Routes>
         <Route path="/" element={<Storefront />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/my-orders" element={<CustomerOrdersPage />} />
-        <Route path="/merchant/dashboard" element={<MerchantDashboard />} />
-        <Route path="/merchant/add-product" element={<AddProduct />} />
-        <Route path="/merchant/edit-product/:id" element={<EditProduct />} />
+        <Route path="/orders" element={<CustomerOrdersPage />} />
+        <Route path="/merchant" element={<MerchantDashboard />} />
         <Route path="/admin/orders" element={<AdminOrdersPage />} />
+        <Route path="/merchant/add" element={<AddProduct />} />
+        <Route path="/merchant/edit/:productId" element={<EditProduct />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
